da10e3898f49eb37b9671f926722a580953f6beb
1noseA <1noseA2L@gmail.com>
2021-10-04 00:36:26 +0900

    いろいろとうまくいってない

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,27 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 	public static void main(String[] args) throws SQLException {
 
-		Employee emp = findEmployee(7369);
-		System.out.println(emp);
+		// empno = 7369のデータを取得
+//		Employee emp = findEmployee(7369);
+//		System.out.println(emp);
+
+		// deptno = 10の部署に所属する全員を取得
+		// Take1
+//		Department dpt = findDeptno(10);
+//		System.out.println(dpt);
+		// Take2
+//		Employee emp = findDeptno(10);
+//		System.out.println(emp);
+		List<Employee> list = findDeptno(10);
+		for (Employee emp : list) {
+			System.out.println(emp);
+		}
 
 	}
 
@@ -41,6 +56,48 @@ public class Main {
 				emp.setSalary(rs.getInt("salary"));
 				emp.setDeptno(rs.getInt("deptno"));
 				return emp;
+			}
+		} finally {
+			con.close();
+		}
+
+		return null;
+
+	}
+
+	private static List<Employee> findDeptno(int deptno) throws SQLException {
+
+		String url = "jdbc:mysql://localhost:3306/employee";
+		String user = "root";
+		String password = "password";
+
+		Connection con = DriverManager.getConnection(url, user, password);
+
+		// これじゃダメじゃん
+		// PreparedStatement ps = con.prepareStatement("select * from department where deptno = ?");
+		PreparedStatement ps = con.prepareStatement(
+				"select empno, ename, job, hiredate, salary, e.deptno from employee e join department d on e.deptno = d.deptno where e.deptno = ?");
+		ps.setInt(1, deptno);
+		ResultSet rs = ps.executeQuery();
+
+		try {
+			while (rs.next()) {
+//				Department dpt = new Department();
+//				dpt.setDeptno(rs.getInt("deptno"));
+//				dpt.setDname(rs.getString("dname"));
+//				dpt.setLocation(rs.getString("location"));
+//				return dpt;
+				List<Employee> list = new ArrayList<>();
+				for (Employee emp : list) {
+					emp.setEmpno(rs.getInt("empno"));
+					emp.setEname(rs.getString("ename"));
+					emp.setJob(rs.getString("job"));
+					emp.setHiredate(rs.getDate("hiredate"));
+					emp.setSalary(rs.getInt("salary"));
+					emp.setDeptno(rs.getInt("deptno"));
+					list.add(emp);
+				}
+				return list;
 			}
 		} finally {
 			con.close();
